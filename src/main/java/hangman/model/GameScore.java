7d077275f4e8580e27b3a855b928ab40fb30da51
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2021-09-02 07:55:34 -0500

    Insercion de cascarones

---
+++ src/main/java/hangman/model/GameScore.java
@@ -0,0 +1,10 @@
+package hangman.model;
+
+public abstract class GameScore {
+    
+    protected int scoreCalculated;
+
+    public int calculateScore(int correctCount, int incorrectCount){
+        return scoreCalculated;
+    }
+}
---
+++ src/main/java/hangman/model/OriginalScore.java
@@ -0,0 +1,8 @@
+package hangman.model;
+
+public class OriginalScore extends GameScore {
+
+    public int calculateScore(int correctCount, int incorrectCount){
+        this.scoreCalculated = 100;
+    }
+}
