e8be3e4240d69cba70ee6facd7afcfed61202ad1
cristian castellanos <cristian.castellanos@mail.escuelaing.edu.co>
2021-09-02 08:53:02 -0500

    BonusScore implementado

--- src/main/java/hangman/model/BonusScore.java
+++ src/main/java/hangman/model/BonusScore.java
@@ -1,5 +1,18 @@
 package hangman.model;
 
-public class BonusScore {
+public class BonusScore extends GameScore{
     
+    public int calculateScore(int correctCount, int incorrectCount) throws scoreException{
+        scoreCalculated = 0;
+        if(correctCount>0){
+            scoreCalculated += 10*correctCount;
+        }
+        else if(incorrectCount>0){
+            scoreCalculated -= 5*incorrectCount;
+        }
+        if(scoreCalculated < 0){
+            throw new scoreException("El scores es invalido, puntaje negativo");
+        }
+        return scoreCalculated;
+    }
 }
